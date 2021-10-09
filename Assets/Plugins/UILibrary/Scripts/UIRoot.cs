f74063d555f9405527589dae91118b9d78ecc244
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 23:52:08 +0900

    ルートオブジェクトクラスをとりあえず定義

---
+++ Assets/Plugins/UILibrary/Scripts/UIRoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UILibrary
+{
+    /// <summary>
+    /// UI描画のルートオブジェクトインタフェース
+    /// </summary>
+    public interface IUIRoot
+    {
+    }
+
+    /// <summary>
+    /// UI描画のルートオブジェクト
+    /// </summary>
+    [AddComponentMenu("UILibrary/UIRoot")]
+    public class UIRoot : MonoBehaviour
+    {
+    }
+}
