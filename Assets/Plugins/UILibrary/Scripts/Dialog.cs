753ab7eec8a119a75223fae36e19a9289f4bc3d8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 00:06:33 +0900

    ダイアログインタフェース定義

---
+++ Assets/Plugins/UILibrary/Scripts/Dialog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+namespace UILibrary
+{
+    /// <summary>
+    /// ダイアログインタフェース
+    /// </summary>
+    public interface IDialog
+    {
+        /// <summary>
+        /// 閉じられた
+        /// </summary>
+        IObservable<Unit> OnClose { get; }
+    }
+}
