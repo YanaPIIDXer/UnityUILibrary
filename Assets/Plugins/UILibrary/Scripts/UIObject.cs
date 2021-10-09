8c1247acddb6d3a4d070a9fe0467ed2f031754b9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 00:03:19 +0900

    子を追加する処理を実装

--- Assets/Plugins/UILibrary/Scripts/UIObject.cs
+++ Assets/Plugins/UILibrary/Scripts/UIObject.cs
@@ -9,5 +9,16 @@ namespace UILibrary
     /// </summary>
     public abstract class UIObject : MonoBehaviour
     {
+        /// <summary>
+        /// 子を追加
+        /// </summary>
+        /// <param name="childObject">子オブジェクト</param>
+        /// <param name="position">表示する座標</param>
+        /// <param name="zOrder">描画優先度</param>
+        public void AddChild(UIObject childObject, Vector2 position, int zOrder = 0)
+        {
+            childObject.transform.parent = transform;
+            childObject.transform.localPosition = new Vector3(position.x, position.y, zOrder);
+        }
     }
 }
--- Assets/Plugins/UILibrary/Scripts/UIRoot.cs
+++ Assets/Plugins/UILibrary/Scripts/UIRoot.cs
@@ -9,13 +9,20 @@ namespace UILibrary
     /// </summary>
     public interface IUIRoot
     {
+        /// <summary>
+        /// 子を追加
+        /// </summary>
+        /// <param name="childObject">子オブジェクト</param>
+        /// <param name="position">表示する座標</param>
+        /// <param name="zOrder">描画優先度</param>
+        void AddChild(UIObject childObject, Vector2 position, int zOrder = 0);
     }
 
     /// <summary>
     /// UI描画のルートオブジェクト
     /// </summary>
     [AddComponentMenu("UILibrary/UIRoot")]
-    public class UIRoot : MonoBehaviour
+    public class UIRoot : UIObject, IUIRoot
     {
     }
 }
