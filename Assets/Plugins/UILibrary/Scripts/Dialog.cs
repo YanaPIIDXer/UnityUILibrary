4ef20160c65f399f7c332438c79345676c52e106
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 00:41:06 +0900

    再生・終了アニメーションの仕組みを実装

--- Assets/Plugins/UILibrary/Scripts/Dialog.cs
+++ Assets/Plugins/UILibrary/Scripts/Dialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using System;
+using UILibrary.Animation;
 
 namespace UILibrary
 {
@@ -15,6 +16,11 @@ namespace UILibrary
         /// 閉じられた
         /// </summary>
         IObservable<Unit> OnClose { get; }
+
+        /// <summary>
+        /// 閉じる
+        /// </summary>
+        void Close();
     }
 
     /// <summary>
@@ -32,9 +38,44 @@ namespace UILibrary
         /// </summary>
         private Subject<Unit> onCloseSubject = new Subject<Unit>();
 
+        /// <summary>
+        /// 開くアニメーション
+        /// </summary>
+        [SerializeField]
+        private TweenList OpenTweens = null;
+
+        /// <summary>
+        /// 閉じるアニメーション
+        /// </summary>
+        [SerializeField]
+        private TweenList CloseTweens = null;
+
+        void Start()
+        {
+            if (OpenTweens != null)
+            {
+                OpenTweens.Play();
+            }
+        }
+
         void OnDestroy()
         {
             onCloseSubject.OnNext(Unit.Default);
+        }
+
+        /// <summary>
+        /// 閉じる
+        /// </summary>
+        public void Close()
+        {
+            if (CloseTweens == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            CloseTweens.OnFinish = () => Destroy(gameObject);
+            CloseTweens.Play();
         }
     }
 }
