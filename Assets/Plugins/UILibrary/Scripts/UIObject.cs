a88bedcab814bf921c694e05a010922f9d7fc733
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 23:56:50 +0900

    UIオブジェクトComponentをとりあえず定義

---
+++ Assets/Plugins/UILibrary/Scripts/UIObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UILibrary
+{
+    /// <summary>
+    /// UIオブジェクト
+    /// </summary>
+    public abstract class UIObject : MonoBehaviour
+    {
+    }
+}
