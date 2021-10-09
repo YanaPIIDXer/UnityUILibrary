7b62dda5cb9c2ee2aea6e1fc7c6321e6e82fc105
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 00:36:22 +0900

    Tweenリストクラス定義

---
+++ Assets/Plugins/UILibrary/Scripts/Animation/TweenList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace UILibrary.Animation
+{
+    /// <summary>
+    /// Tweenリスト
+    /// </summary>
+    [AddComponentMenu("UILibrary/Animation/TweenList")]
+    public class TweenList : MonoBehaviour
+    {
+        /// <summary>
+        /// Tweenリスト
+        /// </summary>
+        [SerializeField]
+        private List<AnimationTween> Tweens = new List<AnimationTween>();
+
+        /// <summary>
+        /// 終了Action
+        /// </summary>
+        public Action OnFinish { set; private get; }
+
+        /// <summary>
+        /// 再生中？
+        /// </summary>
+        private bool bIsPlaying = false;
+
+        /// <summary>
+        /// 再生
+        /// </summary>
+        public void Play()
+        {
+            bIsPlaying = true;
+            foreach (var tween in Tweens)
+            {
+                tween.Play();
+            }
+        }
+
+        void Update()
+        {
+            if (!bIsPlaying) { return; }
+
+            foreach (var tween in Tweens)
+            {
+                if (!tween.IsFinished) { return; }
+            }
+
+            // 全部終了していたらここに来る
+            bIsPlaying = false;
+            OnFinish?.Invoke();
+        }
+    }
+}
