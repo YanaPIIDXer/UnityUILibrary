e5a722274c0b9e41d8cb787b056d9fd62940a61e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 01:37:13 +0900

    ダイアログ表示処理

---
+++ Assets/Sandbox/Scripts/Sequence.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UILibrary;
+using UILibrary.Util;
+using UniRx.Triggers;
+using System;
+
+/// <summary>
+/// シーケンス
+/// </summary>
+public class Sequence : MonoBehaviour
+{
+    /// <summary>
+    /// ダイアログ表示中？
+    /// </summary>
+    private bool bShownDialog = false;
+
+    /// <summary>
+    /// UIのルート
+    /// </summary>
+    [SerializeField]
+    private UIRoot uiRoot = null;
+
+    void Awake()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => !bShownDialog && Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ =>
+            {
+                try
+                {
+                    TestDialog dialog = PrefabManager.Instance.Instantiate<TestDialog>("TestDialog");
+                    dialog.OnClose.Subscribe(__ => bShownDialog = false).AddTo(gameObject);
+                    uiRoot.AddChild(dialog, Vector3.zero);
+                    bShownDialog = true;
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }).AddTo(gameObject);
+    }
+}
