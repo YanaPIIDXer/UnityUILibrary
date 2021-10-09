2bf3b1474033f85c9473461d2b92693e364ca3b2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 01:28:37 +0900

    テスト用のダイアログを用意

---
+++ Assets/Sandbox/Scripts/TestDialog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UILibrary;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// 実験用ダイアログ
+/// </summary>
+public class TestDialog : Dialog
+{
+    /// <summary>
+    /// 閉じるボタン
+    /// </summary>
+    [SerializeField]
+    private Button closeButton = null;
+
+    void Awake()
+    {
+        closeButton.OnClickAsObservable()
+                   .Subscribe(_ => Close())
+                   .AddTo(gameObject);
+    }
+}
